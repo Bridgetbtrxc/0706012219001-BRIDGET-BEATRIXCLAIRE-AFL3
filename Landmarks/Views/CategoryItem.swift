21a2695898049d4fce027191d541d06d64b3c7e0
Bridgetbtrxc <89723805+Bridgetbtrxc@users.noreply.github.com>
2024-05-03 08:40:45 +0700

    Composing complex interface
    Add navigation between sections

--- Landmarks/Views/CategoryItem.swift
+++ Landmarks/Views/CategoryItem.swift
@@ -1,36 +1,26 @@
 import SwiftUI
 
 
-struct CategoryRow: View {
-    var categoryName: String
-    var items: [Landmark]
+struct CategoryItem: View {
+    var landmark: Landmark
 
 
     var body: some View {
         VStack(alignment: .leading) {
-            Text(categoryName)
-                .font(.headline)
-                .padding(.leading, 15)
-                .padding(.top, 5)
-
-
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(alignment: .top, spacing: 0) {
-                    ForEach(items) { landmark in
-                        CategoryItem(landmark: landmark)
-                    }
-                }
-            }
-            .frame(height: 185)
+            landmark.image
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .foregroundStyle(.primary)
+                .font(.caption)
         }
+        .padding(.leading, 15)
     }
 }
 
 
 #Preview {
-    let landmarks = ModelData().landmarks
-    return CategoryRow(
-        categoryName: landmarks[0].category.rawValue,
-        items: Array(landmarks.prefix(4))
-    )
+    CategoryItem(landmark: ModelData().landmarks[0])
 }
